7e0482c293bddbad0d21164eaabf717e1e1d221b
Cedrick Alegroso <42290274+itsdancedrick@users.noreply.github.com>
2019-08-15 11:19:18 +0800

    Create hastag.js

---
+++ hastag.js
@@ -0,0 +1,13 @@
+let readlineSync = require('readline-sync');
+
+let test1
+
+word = readlineSync.question('').split("#");
+
+while(!test1)
+if (word == word.toUpperCase) {
+    console.log("Uppercase")
+} else {
+    console.log("Nope")
+}
+
